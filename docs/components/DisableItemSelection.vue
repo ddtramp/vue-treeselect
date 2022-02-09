a768d8d66d18ec100efe1abdfe6858497759480b
jack wang <xichao.wang@phengine.com>
2022-02-09 17:40:01 +0800

    node add disabled option; update docs

--- docs/components/DisableItemSelection.vue
+++ docs/components/DisableItemSelection.vue
@@ -1,42 +1,81 @@
 <template>
-  <treeselect
-    :multiple="true"
-    :options="options"
-    :value="value"
-    />
+  <treeselect :multiple="false" :options="options" :value="value" />
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      options: [ {
-        id: 'folder',
-        label: 'Normal Folder',
-        children: [ {
-          id: 'disabled-checked',
-          label: 'Checked',
-          isDisabled: true,
-        }, {
-          id: 'disabled-unchecked',
-          label: 'Unchecked',
-          isDisabled: true,
-        }, {
-          id: 'item-1',
-          label: 'Item',
-        } ],
-      }, {
-        id: 'disabled-folder',
-        label: 'Disabled Folder',
+/* eslint-disable */
+export default {
+  data: () => ({
+    options: [
+      {
+        id: "folder",
+        label: "Normal Folder",
+        children: [
+          {
+            id: "disabled-checked",
+            label: "Checked",
+            isDisabled: true,
+          },
+          {
+            id: "disabled-unchecked",
+            label: "Unchecked",
+            isDisabled: true,
+          },
+          {
+            id: "item-1",
+            label: "Item",
+          },
+        ],
+      },
+      {
+        id: "disabled-folder",
+        label: "Disabled Folder",
         isDisabled: true,
-        children: [ {
-          id: 'item-2',
-          label: 'Item',
-        }, {
-          id: 'item-3',
-          label: 'Item',
-        } ],
-      } ],
-      value: [ 'disabled-checked' ],
-    }),
-  }
+        children: [
+          {
+            id: "item-2",
+            label: "Item",
+          },
+          {
+            id: "item-3",
+            label: "Item",
+          },
+        ],
+      },
+
+      {
+        id: "disabled-folder-3",
+        label: "Disabled Folder",
+        disabled: true,
+        children: [
+          {
+            id: "item-22",
+            label: "Item",
+          },
+          {
+            id: "item-33",
+            label: "Item",
+          },
+        ],
+      },
+
+      {
+        id: "disabled-folder-4",
+        label: "Disabled Folder",
+        children: [
+          {
+            id: "item-44",
+            label: "Item",
+            disabled: true,
+          },
+          {
+            id: "item-55",
+            label: "Item",
+          },
+        ],
+      },
+    ],
+    value: "disabled-checked",
+  }),
+};
 </script>
